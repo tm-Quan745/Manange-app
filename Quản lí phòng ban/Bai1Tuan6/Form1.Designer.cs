66cc303fe1b5d2bba4099336205f57775693271d
tm-Quan745 <tmquan.thpt.pvd2023@gmail.com>
2025-03-22 22:56:47 +0700

    add QLPB

---
+++ Quản lí phòng ban/Bai1Tuan6/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Bai1Tuan6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_MaPB = new System.Windows.Forms.TextBox();
+            this.txt_TenPB = new System.Windows.Forms.TextBox();
+            this.btn_TimKiem = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.txt_TimKiem = new System.Windows.Forms.TextBox();
+            this.rtx_MoTa = new System.Windows.Forms.RichTextBox();
+            this.dgr_PhongBan = new System.Windows.Forms.DataGridView();
+            this.col_MaPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_TenPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_MoTa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgr_PhongBan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Phòng Ban:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Phòng Ban:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mô tả:";
+            // 
+            // txt_MaPB
+            // 
+            this.txt_MaPB.Location = new System.Drawing.Point(183, 89);
+            this.txt_MaPB.Name = "txt_MaPB";
+            this.txt_MaPB.Size = new System.Drawing.Size(230, 22);
+            this.txt_MaPB.TabIndex = 3;
+            // 
+            // txt_TenPB
+            // 
+            this.txt_TenPB.Location = new System.Drawing.Point(183, 139);
+            this.txt_TenPB.Name = "txt_TenPB";
+            this.txt_TenPB.Size = new System.Drawing.Size(230, 22);
+            this.txt_TenPB.TabIndex = 4;
+            // 
+            // btn_TimKiem
+            // 
+            this.btn_TimKiem.Location = new System.Drawing.Point(547, 37);
+            this.btn_TimKiem.Name = "btn_TimKiem";
+            this.btn_TimKiem.Size = new System.Drawing.Size(93, 28);
+            this.btn_TimKiem.TabIndex = 5;
+            this.btn_TimKiem.Text = "Tìm kiếm";
+            this.btn_TimKiem.UseVisualStyleBackColor = true;
+            this.btn_TimKiem.Click += new System.EventHandler(this.btn_TimKiem_Click);
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Location = new System.Drawing.Point(547, 105);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(93, 28);
+            this.btn_Them.TabIndex = 6;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.Click += new System.EventHandler(this.btn_Them_Click);
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Location = new System.Drawing.Point(547, 146);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(93, 28);
+            this.btn_Sua.TabIndex = 7;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            this.btn_Sua.Click += new System.EventHandler(this.btn_Sua_Click);
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Location = new System.Drawing.Point(547, 215);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(93, 28);
+            this.btn_Xoa.TabIndex = 8;
+            this.btn_Xoa.Text = "Xoá";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            this.btn_Xoa.Click += new System.EventHandler(this.btn_Xoa_Click);
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Location = new System.Drawing.Point(547, 249);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(93, 28);
+            this.btn_Thoat.TabIndex = 9;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // txt_TimKiem
+            // 
+            this.txt_TimKiem.Location = new System.Drawing.Point(183, 37);
+            this.txt_TimKiem.Name = "txt_TimKiem";
+            this.txt_TimKiem.Size = new System.Drawing.Size(324, 22);
+            this.txt_TimKiem.TabIndex = 10;
+            this.txt_TimKiem.TextChanged += new System.EventHandler(this.txt_TimKiem_TextChanged);
+            // 
+            // rtx_MoTa
+            // 
+            this.rtx_MoTa.Location = new System.Drawing.Point(183, 202);
+            this.rtx_MoTa.Name = "rtx_MoTa";
+            this.rtx_MoTa.Size = new System.Drawing.Size(324, 84);
+            this.rtx_MoTa.TabIndex = 11;
+            this.rtx_MoTa.Text = "";
+            // 
+            // dgr_PhongBan
+            // 
+            this.dgr_PhongBan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgr_PhongBan.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_MaPhong,
+            this.col_TenPhong,
+            this.col_MoTa});
+            this.dgr_PhongBan.Location = new System.Drawing.Point(31, 314);
+            this.dgr_PhongBan.Name = "dgr_PhongBan";
+            this.dgr_PhongBan.RowHeadersWidth = 51;
+            this.dgr_PhongBan.RowTemplate.Height = 24;
+            this.dgr_PhongBan.Size = new System.Drawing.Size(681, 150);
+            this.dgr_PhongBan.TabIndex = 12;
+            this.dgr_PhongBan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgr_PhongBan_CellContentClick);
+            // 
+            // col_MaPhong
+            // 
+            this.col_MaPhong.DataPropertyName = "MaPB";
+            this.col_MaPhong.HeaderText = "Mã phòng";
+            this.col_MaPhong.MinimumWidth = 6;
+            this.col_MaPhong.Name = "col_MaPhong";
+            this.col_MaPhong.Width = 125;
+            // 
+            // col_TenPhong
+            // 
+            this.col_TenPhong.DataPropertyName = "TenPB";
+            this.col_TenPhong.HeaderText = "Tên phòng";
+            this.col_TenPhong.MinimumWidth = 6;
+            this.col_TenPhong.Name = "col_TenPhong";
+            this.col_TenPhong.Width = 125;
+            // 
+            // col_MoTa
+            // 
+            this.col_MoTa.DataPropertyName = "MoTa";
+            this.col_MoTa.HeaderText = "Mô tả";
+            this.col_MoTa.MinimumWidth = 6;
+            this.col_MoTa.Name = "col_MoTa";
+            this.col_MoTa.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 487);
+            this.Controls.Add(this.dgr_PhongBan);
+            this.Controls.Add(this.rtx_MoTa);
+            this.Controls.Add(this.txt_TimKiem);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.btn_TimKiem);
+            this.Controls.Add(this.txt_TenPB);
+            this.Controls.Add(this.txt_MaPB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Phòng Ban";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgr_PhongBan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_MaPB;
+        private System.Windows.Forms.TextBox txt_TenPB;
+        private System.Windows.Forms.Button btn_TimKiem;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.TextBox txt_TimKiem;
+        private System.Windows.Forms.RichTextBox rtx_MoTa;
+        private System.Windows.Forms.DataGridView dgr_PhongBan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_MaPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_TenPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_MoTa;
+    }
+}
+
---
+++ Quản lí phòng ban/Bai1Tuan6/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1Tuan6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        KetNoiDuLieu kn = new KetNoiDuLieu();
+
+        public void LoadData()
+        {
+            string sql = "SELECT * FROM PhongBan";
+            dgr_PhongBan.DataSource = kn.TaoBang(sql);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kn.MyConnect();
+            txt_MaPB.ResetText();
+            txt_TenPB.ResetText();
+            rtx_MoTa.ResetText();
+            LoadData();
+        }
+
+        private void dgr_PhongBan_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int chiso = -1;
+            DataTable bang = new DataTable();
+            bang = (DataTable)dgr_PhongBan.DataSource;
+            chiso = dgr_PhongBan.SelectedCells[0].RowIndex;
+            DataRow hang = bang.Rows[chiso];
+
+            txt_MaPB.Text = hang["MaPB"].ToString().Trim();
+            txt_TenPB.Text = hang["TenPB"].ToString().Trim();
+            rtx_MoTa.Text = hang["MoTa"].ToString().Trim();
+            LoadData();
+
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            kn.MyDisconnect();
+            this.Close();
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            string s = "SELECT * FROM PhongBan where MaPB='" + txt_MaPB.Text + "'";
+            DataTable dt = new DataTable();
+            dt = kn.TaoBang(s);
+            if (dt.Rows.Count == 0)
+            {
+                kn.them(txt_MaPB.Text,txt_TenPB.Text,rtx_MoTa.Text);
+                txt_MaPB.Focus();
+                MessageBox.Show("Thêm thành công!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else
+            {
+                MessageBox.Show("Mã số sinh viên đã có, vui lòng nhập lại", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            txt_MaPB.ResetText();
+            txt_TenPB.ResetText();
+            rtx_MoTa.ResetText();
+            txt_MaPB.Focus();
+            LoadData();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            string s = "SELECT * FROM PhongBan where MaPB='" + txt_MaPB.Text + "'";
+            DataTable dt = new DataTable();
+            dt = kn.TaoBang(s);
+            if (dt.Rows.Count != 0)
+            {
+                kn.sua(txt_MaPB.Text, txt_TenPB.Text, rtx_MoTa.Text);
+                MessageBox.Show("Cập nhật thành công!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txt_MaPB.ResetText();
+                txt_TenPB.ResetText();
+                rtx_MoTa.ResetText();
+            }
+            else
+            {
+                MessageBox.Show("Bạn chưa chọn dữ liệu để sửa", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            LoadData();
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            string s = "SELECT * FROM PhongBan where MaPB='" + txt_MaPB.Text + "'";
+            DataTable dt = new DataTable();
+            dt = kn.TaoBang(s);
+            if (dt.Rows.Count != 0)
+            {
+                kn.xoa(txt_MaPB.Text, txt_TenPB.Text, rtx_MoTa.Text);
+                MessageBox.Show("Xoá thành công!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txt_MaPB.ResetText();
+                txt_TenPB.ResetText();
+                rtx_MoTa.ResetText();
+
+            }
+            else
+            {
+                MessageBox.Show("Bạn chưa chọn dữ liệu để xoá", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            LoadData();
+        }
+
+        private void btn_TimKiem_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM PhongBan WHERE TenPB = @TenPB"; // Use parameter instead of direct concatenation
+            dgr_PhongBan.DataSource = kn.TimKiemPhongBan(sql, txt_TimKiem.Text);
+        }
+        
+        private void txt_TimKiem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+public class KetNoiDuLieu
+{
+    SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Quanlinhanvien;Integrated Security=True;TrustServerCertificate=True");
+    public void MyConnect()
+    {
+        conn.Open();
+    }
+    public void MyDisconnect()
+    {
+        conn.Close();
+    }
+    public DataTable TaoBang(string sql)
+    {
+        DataTable dt = new DataTable();
+        SqlDataAdapter ds = new SqlDataAdapter(sql, conn);
+        ds.Fill(dt);
+        return dt;
+
+    }
+    public void them(string maPB, string tenPB, string moTa)
+    {
+        string sql = "INSERT INTO PhongBan(MaPB,TenPB,MoTa)" +
+            "values('" + maPB + "',N'" + tenPB + "',N'" + moTa + "')";
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+    public void sua(string maPB, string tenPB, string moTa)
+    {
+        string sql = "UPDATE PhongBan SET TenPB=N'" + tenPB + "',MoTa=N'" + moTa + "' WHERE MaPB='" + maPB + "'";
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+    public void xoa(string maPB, string tenPB, string moTa)
+    {
+        string sql = "DELETE FROM PhongBan WHERE MaPB ='" + maPB + "'";
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        cmd.ExecuteNonQuery();
+    }
+    public DataTable TimKiemPhongBan(string sql, string tenPB)
+    {
+        DataTable dt = new DataTable();
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@TenPB", tenPB);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        return dt;
+    }
+}
+
+
+   
